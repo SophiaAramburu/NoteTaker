ae8d2c8295ae23c24614507b14568e8a7a6d95e7
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-18 20:26:41 -0800

    setting up server

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const util = require("utils");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
