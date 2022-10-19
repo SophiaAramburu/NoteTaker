efc54fd5fbc8f9d4b7752e0a116de542601260d0
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-18 20:23:18 -0800

    added asynchronos files

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const util = require("utils");
+
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
