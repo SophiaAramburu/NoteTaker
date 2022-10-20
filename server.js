ebe8bbfa78e39e4f12411b628c0ed9128214275d
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-19 19:40:49 -0800

    added route folder and file

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const util = require("utils");
 
-
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-app.use(express.static("./public"));
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
 
+
+
+require('./routes/routes.js')(app);
 app.get("/api/notes", function(req, res) {
 readFileAsync("./db/json", "utf8").then(function(data) {
     notes = [].concat(JSON.parse(data))
@@ -35,6 +37,19 @@ return notes
 })
 });
 
+app.delete("/api/notes/:id", function(req, res) {
+    const idToDelete = parseInt(req.params.id);
+    readFileAsync("/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        const newNotesData = []
+        for (let i = 0; i<notes.length; i++) {
+            if(idToDelete !== notes[i].id) {
+                newNotesData.push(notes[i])
+            }
+        }
+    })
+})
+
 app.get("/notes", function(req, res) {
 res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
@@ -47,6 +62,3 @@ app.get("*", function(req, res) {
 res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.listen(PORT, function() {
-console.log("app is listening on PORT " + PORT);
-});
