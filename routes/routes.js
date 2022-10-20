f5e5bc9a78b7a62c77dda7edca39e52a28f69e6c
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-19 19:41:39 -0800

    added if statment and error

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,8 @@ const path = require('path');
 
 module.exports = app => {
 
-    
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
