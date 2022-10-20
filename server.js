add44771652e60c569aaf08301e8923b9e07236b
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-19 19:50:51 -0800

    cleaned up code and organized better and added bonus point

--- server.js
+++ server.js
@@ -10,55 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
+require('./routes/routes')(app);
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
-
-
-
-require('./routes/routes.js')(app);
-app.get("/api/notes", function(req, res) {
-readFileAsync("./db/json", "utf8").then(function(data) {
-    notes = [].concat(JSON.parse(data))
-    res.json(notes);
-})
-})
-
-app.post("/api/notes", function(req, res) {
-const note = req.body;
-readFileAsync("./db/db.json", "utf8").then(function(data) {
-const notes = [].concat(JSON.parse(data));
-note.id = notes.length + 1
-notes.push(note);
-return notes
-}).then(function(notes) {
-    writeFileAsync("./db/db.json", JSON.stringify(notes))
-    res.json(note);
-})
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-    const idToDelete = parseInt(req.params.id);
-    readFileAsync("/db/db.json", "utf8").then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        const newNotesData = []
-        for (let i = 0; i<notes.length; i++) {
-            if(idToDelete !== notes[i].id) {
-                newNotesData.push(notes[i])
-            }
-        }
-    })
-})
-
-app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("*", function(req, res) {
-res.sendFile(path.join(__dirname, "./public/index.html"));
-});
 
